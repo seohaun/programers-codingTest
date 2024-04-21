ace1eb2b7d3d416caa7f202a5d7078369d50d469
seosuguen <skyhighzz@naver.com>
2024-04-21 14:29:40 +0900

    feat> 이어 붙인 수

---
+++ src/basicTest/ConcatenatedNumber.java
@@ -0,0 +1,43 @@
+package basicTest;
+/*
+ https://school.programmers.co.kr/learn/courses/30/lessons/181928
+ 이어 붙인 수
+ - 정수가 담긴 리스트 num_list가 주어집니다.
+ - num_list의 홀수만 순서대로 이어 붙인 수와 짝수만 순서대로 이어 붙인 수의 합을 return하도록 solution 함수를 완성해주세요.
+ 제한사항
+ - 2 ≤ num_list의 길이 ≤ 10
+ - 1 ≤ num_list의 원소 ≤ 9
+ - num_list에는 적어도 한 개씩의 짝수와 홀수가 있습니다.
+ 입출력 예
+ - num_list	        result
+ - [3, 4, 5, 2, 1]	393
+ - [5, 7, 8, 3]	    581
+ 입출력 예 설명
+ - 입출력 예 #1 : 홀수만 이어 붙인 수는 351이고 짝수만 이어 붙인 수는 42입니다. 두 수의 합은 393입니다.
+ - 입출력 예 #2 : 홀수만 이어 붙인 수는 573이고 짝수만 이어 붙인 수는 8입니다. 두 수의 합은 581입니다.
+ */
+public class ConcatenatedNumber {
+
+    public static void main(String[] args) {
+        int[] num_list = {3, 4, 5, 2, 1};
+        int solution = solution(num_list);
+        if (solution == 393) {
+            System.out.println("참");
+        } else {
+            System.out.println("거짓");
+        }
+    }
+
+    public static int solution(int[] num_list) {
+        StringBuilder odd = new StringBuilder();
+        StringBuilder even = new StringBuilder();
+        for(int num : num_list){
+            if(num % 2 == 0){
+                odd.append(num);
+                continue;
+            }
+            even.append(num);
+        }
+        return Integer.parseInt(odd.toString())+Integer.parseInt(even.toString());
+    }
+}
