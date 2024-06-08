59df5c72a94f12395063823023857634442adf76
seohaun <skyhighzz@naver.com>
2024-06-08 12:44:07 +0900

    feat. 숨어있는 숫자의 덧셈

--- src/test/introducton/AdditionOfHiddenNumbersTest.java
+++ src/test/introducton/AdditionOfHiddenNumbersTest.java
@@ -4,13 +4,11 @@ import introduction.AdditionOfHiddenNumbers;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class AdditionOfHiddenNumbersTest {
 
     @Test
     void solution() {
         AdditionOfHiddenNumbers additionOfHiddenNumbers = new AdditionOfHiddenNumbers();
-        Assertions.assertThat(additionOfHiddenNumbers.solution("aAb1B2cC34oOp")).isEqualTo(10);
+        Assertions.assertThat(AdditionOfHiddenNumbers.solution("aAb1B2cC34oOp")).isEqualTo(10);
     }
 }
